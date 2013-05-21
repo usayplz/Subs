f302bf2b907824f48d9ef471244429e4ac3291e1
pandozer <mrpandozer@gmail.com>
2013-05-21 21:10:25 +0900

    hello sms

--- scripts/sender.py
+++ scripts/sender.py
@@ -4,13 +4,18 @@ import logging
 from twisted.internet import reactor, defer
 from smpp.twisted.client import SMPPClientTransceiver, SMPPClientService
 from smpp.twisted.config import SMPPClientConfig
+from smpp.pdu.error import *
+from smpp.pdu.operations import *
+from smpp.pdu.pdu_types import *
+
 
 class SMPP(object):
+    sme_num = '8181'
 
     def __init__(self, config=None):
         if config is None:
             config = SMPPClientConfig(host='81.18.113.146', port=3202, username='272', 
-            	password='Ha33sofT')
+            	password='Ha33sofT', )
         self.config = config
 
     @defer.inlineCallbacks
@@ -24,9 +29,22 @@ class SMPP(object):
             reactor.stop()
 
     def handleMsg(self, smpp, pdu):
-        print "Received pdu %s" % pdu
+        print "Received pdu: %s" % pdu
+        print "DEBUG :: %s" % pdu.commandId
+
+        if pdu.commandId == CommandId.deliver_sm:
+            submit_pdu = SubmitSM(
+                source_addr=self.sme_num,
+                destination_addr=pdu.params['source_addr'],
+                short_message='HELLO',
+                source_addr_ton=AddrTon.ALPHANUMERIC,
+                dest_addr_ton=AddrTon.INTERNATIONAL,
+                dest_addr_npi=AddrNpi.ISDN,
+            )
+            smpp.sendDataRequest(submit_pdu)
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     SMPP().run()
     reactor.run()
