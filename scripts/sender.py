d2c307f91174917bb73a4e54a5e3b8112159b96d
pandozer <mrpandozer@gmail.com>
2013-05-22 08:06:52 +0900

    send_sms

--- scripts/sender.py
+++ scripts/sender.py
@@ -14,8 +14,7 @@ class SMPP(object):
 
     def __init__(self, config=None):
         if config is None:
-            config = SMPPClientConfig(host='81.18.113.146', port=3202, username='272', 
-            	password='Ha33sofT', )
+            config = SMPPClientConfig(host='81.18.113.146', port=3202, username='272', password='Ha33sofT', )
         self.config = config
 
     @defer.inlineCallbacks
@@ -28,20 +27,28 @@ class SMPP(object):
         finally:
             reactor.stop()
 
+    def send_sms(self, source_addr, short_message):
+        submit_pdu = SubmitSM(
+            source_addr = self.sme_num,
+            destination_addr = source_addr,
+            short_message = short_message,
+            source_addr_ton = AddrTon.ALPHANUMERIC,
+            dest_addr_ton = AddrTon.INTERNATIONAL,
+            dest_addr_npi = AddrNpi.ISDN,
+            esm_class = EsmClass(EsmClassMode.DEFAULT, EsmClassType.DEFAULT),
+            protocol_id = 0,
+            registered_delivery = RegisteredDelivery(
+                RegisteredDeliveryReceipt.SMSC_DELIVERY_RECEIPT_REQUESTED),
+                replace_if_present_flag = ReplaceIfPresentFlag.DO_NOT_REPLACE,
+            )
+        smpp.sendDataRequest(submit_pdu)
+
     def handleMsg(self, smpp, pdu):
         print "Received pdu: %s" % pdu
         print "DEBUG :: %s" % pdu.commandId
 
         if pdu.commandId == CommandId.deliver_sm:
-            submit_pdu = SubmitSM(
-                source_addr=self.sme_num,
-                destination_addr=pdu.params['source_addr'],
-                short_message='HELLO',
-                source_addr_ton=AddrTon.ALPHANUMERIC,
-                dest_addr_ton=AddrTon.INTERNATIONAL,
-                dest_addr_npi=AddrNpi.ISDN,
-            )
-            smpp.sendDataRequest(submit_pdu)
+            self.send_sms(pdu.params['source_addr'], 'test')
 
 
 if __name__ == '__main__':
