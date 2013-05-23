05977c9e8fdc0f2e7a777a9a704b8c446f1f9fc8
pandozer <mrpandozer@gmail.com>
2013-05-23 13:26:39 +0000

    dbsmstask.py добавлено выборка погоды

--- scripts/dbsmstask.py
+++ scripts/dbsmstask.py
@@ -5,23 +5,46 @@ import sqlite3 as db
 
 class dbSMSTask(object):
     connection = None
+    connection_state = 0
     cursor = None
+    weather = None
+    weather_time = None
+    weather_timeout = 15*60
 
     def __init__(self, connection_string):
+        self.connection_state = 0
+        return connect(connection_string)
+            
+    def connect(self):
         try:
             self.connection = db.connect(connection_string)
             self.cursor = connection.cursor()
+            self.connection_state = 1
         except db.Error, e:
-            print e
-            return 2
+            self.connection_state = 0
+            return e
+        return True
 
+    def get_weather(self):
+        sql = '''
+            select sms_test, time from sms_text order by from_time desc limit 1
+        '''
+        try:
+            self.cursor.execute(sql, {})
+        except db.Error, e:
+            self.connection_state = 0
+            return e
+        weather, self.weather_time = self.cursor.fetchone()
+        return weather
+    
     def __del__(self):
         self.connection.commit()
         self.connection.close()
 
     def add_new_task(self, mobnum, message_id):
         status = 0
-        weather = u'good'
+        if not self.weather or time.now()-self.weather_time > self.weather_timeout:
+            self.weather = self.get_weather()
 
         sql = '''
             insert into sender_smstask 
@@ -32,14 +55,15 @@ class dbSMSTask(object):
         try:
             self.cursor.execute(sql, {
                 "mobnum": mobnum,
-                "sms_text": weather,
+                "sms_text": self.weather,
                 "status": status, 
                 "message_id": message_id
             })
             self.connection.commit()
         except db.Error, e:
-            print e
-            return 2
+            self.connection_state = 0
+            return e
+        return True
 
     def update_task_status(self, status, message_id):
         sql = '''
@@ -54,8 +78,9 @@ class dbSMSTask(object):
             })
             self.connection.commit()
         except db.Error, e:
-            print e
-            return 2
+            self.connection_state = 0
+            return e
+        return True
 
 
 def main(args):
