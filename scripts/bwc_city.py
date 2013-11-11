8e0d455ed3d42f5cdedc14c5ea696d270b4f62f0
skurikalov <skurikalov@bwc.ru>
2013-11-11 15:53:57 +0900

    bwc_city urllib2

--- scripts/bwc_city.py
+++ scripts/bwc_city.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib.request
-import urllib.parse
+import urllib2
+import urllib
+
 
 class BWCCity():
     URL = 'https://sasgwapi.bwc.ru/cgi-bin/geotarget.cgi'
@@ -14,14 +15,16 @@ class BWCCity():
         return bwc_code_id
 
     def put_bwc_geotarget(self, mobnum):
-        params = urllib.parse.urlencode({'action': 'put', 'passphrase': PASSPHRASE, 'mob_num': mobnum })
-        request = urllib.request.urlopen("%s?%s" % (URL, params))
-        return request.read().decode('utf-8')
+        params = urllib.urlencode({'action': 'put', 'passphrase': PASSPHRASE, 'mob_num': mobnum })
+        request = urllib2.Request(URL, params, headers)
+        response = urllib2.urlopen(request)
+        return response.read().decode('utf-8')
 
     def get_bwc_geotarget(self, query_id):
-        params = urllib.parse.urlencode({'action': 'get', 'passphrase': PASSPHRASE, 'query_id': query_id })
-        request = urllib.request.urlopen("%s?%s" % (URL, params))
-        return request.read().decode('utf-8')
+        params = urllib.urlencode({'action': 'get', 'passphrase': PASSPHRASE, 'query_id': query_id })
+        request = urllib2.Request(URL, params, headers)
+        response = urllib2.urlopen(request)
+        return response.read().decode('utf-8')
 
 if __name__ == '__main__':
     print BWCCity('9500500600')
