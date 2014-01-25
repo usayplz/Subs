272f44fa1b44cb5c1607b1ea68a34767d7fa4493
pandozer <root@stkfactory>
2014-01-25 11:58:21 +0900

    show weather on main page

--- sender/views.py
+++ sender/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from datetime import datetime
+from models import WeatherText
 
 
 def index(request):
-    # current_weather = SMSText.objects.filter(from_date__lt=datetime.now, to_date__gt=datetime.now)
-    current_weather = u''
+    current_weather = WeatherText.objects.filter(time_from__lt=datetime.now, time_to__gt=datetime.now)
     return render(request, 'sender/index.html', {'current_weather': current_weather})
