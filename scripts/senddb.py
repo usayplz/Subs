9a2a6924dfae08074f65e478ccae776e7e2c6568
pandozer <mrpandozer@gmail.com>
2014-06-02 20:49:47 +0900

    true backup

--- scripts/senddb.py
+++ scripts/senddb.py
@@ -4,6 +4,9 @@
 from mail import send_mail
 import os, time
 
-os.system("mysqldump -u root -p kanefron7 --all-databases > /var/www/subs/scripts/cities2db/subsd b.sql")
-time.sleep(3)
-send_mail('subs@foxthrottle.com', ['metasize@gmail.com'], 'subs', 'database backup', ['/var/www/subs/scripts/cities2db/subsdb.sql'])
+os.system("rm -f /tmp/subs*")
+os.system("mysqldump -uroot -pkanefron7 subsdb > /tmp/subsdb.sql")
+time.sleep(30)
+os.system("bzip2 /tmp/subsdb.sql")
+time.sleep(30)
+send_mail('subs@foxthrottle.com', ['metasize@gmail.com'], 'subs', 'database backup', ['/tmp/subsdb.sql.bz2'])
