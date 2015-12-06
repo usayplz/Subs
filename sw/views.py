99e20527b33e1eaee4c3f480c7c1a47d7b5b84e3
pandozer <mrpandozer@gmail.com>
2015-12-06 09:46:35 +0800

    show weather

--- sw/views.py
+++ sw/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from sw.models import WeatherText
+from datetime import datetime
+
+def index(request):
+	weathers = WeatherText.objects.filter(time_from__lte=datetime.now(), time_to__gte=datetime.now())
+    return render(request, 'sw/index.html', {"weathers": weathers})
 
 def wsdl(request):
     return render(request, 'sw/rt.wsdl', {}, content_type='application/xhtml+xml')
